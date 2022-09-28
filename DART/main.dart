d7f6303343e55fe98c18844e007fd16ae88caf7f
BekzodGayratov <bekzodflutter@gmail.com>
2022-09-29 00:00:02 +0500

    readMe file was added

---
+++ DART/main.dart
@@ -0,0 +1,14 @@
+// Dart dasturlash tili static tipli dasturlash tili hisoblanadi lekin unda "var" ham bor.
+// Dart faylini ishga tushurish quyidagicha bo'ladi.
+// Masalan, "main.dart" nomli dart faylingiz bor.
+// Ishga tushurish uchun "terminalga", "dart run main.dart" skriptini kiritasiz.
+
+/////////////////////////////////////////////////
+// Dart fayli ishga tushganida operatsion tizim quyidagi maydonni o'qiy boshlaydi.
+
+void main() {
+  // Dart fayli ishga tushganida birinchi bo'lib ushbu funksiyani ishga tushuradi.
+  // Endi kodimizni yozishimiz mumkin. Masalan,
+  print("FinTech company");
+  // Qolgan barcha o'zimiz kiritgan funksiyalarimiz shu yerda chaqiriladi.
+}
--- DART/variables/1-dars_variables.dart
+++ DART/variables/1-dars_variables.dart
@@ -9,12 +9,12 @@ int son1 = 10; // Butun sonlarni saqlaydi.
 double son2 = 11.2; // Qoldiqli sonlarni saqlaydi.
 
 String ism =
-    "Abdulloh"; // Manbaa, raqam bo'lmagan ma'lumotlarni, hamda belgilarni saqlaydi
+    "Abdulloh"; // Manbaa, raqam bo'lmagan ma'lumotlarni, hamda belgilarni saqlaydi.
 
 bool ishgaYaroqlimi =
-    false; // Faqat ikkisidan biri ya'ni "HA" yoki "YO'Q" javobini saqlaydi.
+    false; // Faqat ikkisidan biri ya'ni "HA" yoki "YO'Q" qiymatini saqlaydi.
 
 num raqam = 10;
 num raqam2 =
-    11.0; // Int yoki double o'zgaruvchilarini o'zida saqlaydi faqat raqam bo'lsa bo'lgani.
+    11.0; // int yoki double o'zgaruvchilarini o'zida saqlaydi faqat raqam bo'lsa bo'lgani.
 
