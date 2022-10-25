790b0b207a0615616581791c0d4a74d43c2510e5
BekzodGayratov <bekzodflutter@gmail.com>
2022-10-25 10:31:22 +0500

    Getter and setters

---
+++ DART/TUTORIALS/9_dars/get.dart
@@ -0,0 +1,8 @@
+import 'main.dart';
+
+void main() {
+  Student s = Student();
+  s.set_name = "Dart";
+
+  print(s.name);
+}
---
+++ DART/TUTORIALS/9_dars/main.dart
@@ -0,0 +1,14 @@
+void main() {}
+
+class Student {
+  String name = "Flutter";
+  int age = 18;
+
+  String get get_name {
+    return name;
+  }
+
+  void set set_name(String name) {
+    this.name = name;
+  }
+}
