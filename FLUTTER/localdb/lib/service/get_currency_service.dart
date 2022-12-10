afdec6716814df4eb8543362780d39692df2d3d6
BekzodGayratov <bekzodflutter@gmail.com>
2022-12-10 02:55:05 -0600

    localdb

--- FLUTTER/localdb/lib/service/get_currency_service.dart
+++ FLUTTER/localdb/lib/service/get_currency_service.dart
@@ -1,7 +1,11 @@
 import 'package:dio/dio.dart';
+import 'package:hive/hive.dart';
+import 'package:localdb/model/currency_model.dart';
+import 'package:path_provider/path_provider.dart';
 
 class CurrencyService {
-  Future<dynamic> getCurrencyService() async {
+  static late Box<CurrencyModel> currencyBox;
+  static Future<dynamic> getCurrencyService() async {
     try {
       Response response = await Dio().get("");
       if (response.statusCode == 200) {
@@ -12,9 +16,23 @@ class CurrencyService {
       switch (e.type) {
         case DioErrorType.connectTimeout:
           return "connect time out";
-          break;
+        case DioErrorType.receiveTimeout:
+          return "receive time out";
+        case DioErrorType.sendTimeout:
+          return "send time out";
         default:
       }
     }
   }
+
+  static void openBox() async {
+    // 8600 4929 4873 8006
+    var appDocDir = await getApplicationDocumentsDirectory();
+    Hive.init(appDocDir.path);
+    currencyBox = await Hive.openBox<CurrencyModel>("currency");
+  }
+
+  void putToBox() async {}
+
+  void registerAdapters() {}
 }
