09007862aa9df6942cfbed8fcdd5246a0d6d6220
BekzodGayratov <bekzodflutter@gmail.com>
2023-03-24 02:14:37 -0500

    New lessons

--- DART/TUTORIALS/4_dars/alg.dart
+++ DART/TUTORIALS/4_dars/alg.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+
 void main() {
-  aylan();
+  List<int> nums = [12, 44, 55, 66, 32, 5, 8];
+
+  print(bubleSort(nums));
 }
 
 aylan() async {
@@ -8,3 +12,44 @@ aylan() async {
     print(i);
   }
 }
+
+findWord() {
+  String word = stdin.readLineSync()!;
+
+  String infoBase =
+      "The fluttter is best framework, it can supports the cross-platform applications!";
+  if (infoBase.contains(word)) {
+    print("So'z topildi");
+  } else {
+    print("Afsus topilmadi");
+  }
+}
+
+findPalindrom(String name) {
+  String result = "";
+  for (int i = name.length - 1; i >= 0; i--) {
+    result += name[i];
+  }
+
+  if (result.toLowerCase() == name.toLowerCase()) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+bubleSort(List<int> nums) {
+  for (int i = 0; i < nums.length - 1; i++) {
+    int digit = 0;
+    for (int j = 0; j < nums.length - 1; j++) {
+      if (nums[j] > nums[j + 1]) {
+        digit = nums[j];
+        nums[j] = nums[j + 1];
+        nums[j + 1] = digit;
+      }
+    }
+  }
+  return nums;
+}
+
+
